20a6a7c572d7288665af0f600ca9b531624e765a
Abdollatif <a.latifnajafizadeh@gmail.com>
2024-06-16 16:45:30 +0330

    :)

--- src/main/java/com/saman/Form/form/services/impl/FormServiceImpl.java
+++ src/main/java/com/saman/Form/form/services/impl/FormServiceImpl.java
@@ -1,16 +1,26 @@
 package com.saman.Form.form.services.impl;
 
+import com.saman.Form.form.models.dtos.FormDto;
 import com.saman.Form.form.models.request.FormCreateRequest;
 import com.saman.Form.form.services.FormService;
 import com.saman.Form.shared.MyApiResponse;
+import com.saman.Form.utils.FormUtil;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FormServiceImpl implements FormService {
+
+    private final FormUtil formUtil;
+
+    public FormServiceImpl(FormUtil formUtil) {
+        this.formUtil = formUtil;
+    }
+
     @Override
     public ResponseEntity<MyApiResponse> createForm(FormCreateRequest formCreateRequest) {
 
+        FormDto formDto = formUtil.convert(formCreateRequest);
         return null;
     }
 }
--- src/main/java/com/saman/Form/utils/FormUtil.java
+++ src/main/java/com/saman/Form/utils/FormUtil.java
@@ -1,6 +1,42 @@
 package com.saman.Form.utils;
 
+import com.saman.Form.form.models.dtos.FormDto;
+import com.saman.Form.form.models.request.FormCreateRequest;
+import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Random;
+
 public class FormUtil {
+    Logger logger = LoggerFactory.getLogger(FormUtil.class);
 
+    private final ModelMapper modelMapper = new ModelMapper();
 
+    public FormDto convert(FormCreateRequest formCreateRequest) {
+        return createDtoModel(formCreateRequest);
+    }
+
+    private FormDto createDtoModel(FormCreateRequest formCreateRequest) {
+        FormDto formDto = modelMapper.map(formCreateRequest, FormDto.class);
+        formDto.setPublicId(getPublicId());
+        return formDto;
+    }
+
+    private String generateRandomString() {
+        logger.info("GenerateRandomString() has called");
+
+        Random random = new Random();
+        String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder returnValue = new StringBuilder();
+        int length = 32;
+        for (int i = 0; i < length; i++) {
+            returnValue.append(alphabet.charAt(random.nextInt(alphabet.length())));
+        }
+        return returnValue.toString();
+    }
+
+    public String getPublicId() {
+        return generateRandomString();
+    }
 }
